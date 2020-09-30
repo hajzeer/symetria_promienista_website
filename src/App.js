55bca1a2a891b62c57711941a131cffedd309c10
hajzeer <krzysh98@gmail.com>
2020-09-30 21:34:47 +0200

    finish

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
 import React from 'react';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import './App.css';
 
 
 import './App.css';
 import Home from './Pages/Home'
 import Portfolio from './Pages/Portfolio'
+import ScrollToTop from "./Components/ScrollToTop";
 
 const App = () => {
 
 
   return (
       <Router>
-      <div className="App">
-                  <Route path="/" exact component={Home} />
-                  <Route path="/portfolio" component={Portfolio} />
-      </div>
+          <ScrollToTop>
+        <div className="App">
+          <Switch>
+                  <Route  path="/" exact component={Home} />
+                  <Route  path="/portfolio" component={Portfolio} />
+          </Switch>
+        </div>
+          </ScrollToTop>
       </Router>
   );
 }
